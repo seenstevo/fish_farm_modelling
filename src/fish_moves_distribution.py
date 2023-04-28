5c86846101a38be43e46dd6f449e26bc2fc9699d
seenstevo <seenstevo@protonmail.com>
2023-04-28 16:52:22 +0200

    fish moves column shifted and distribution function added

--- src/fish_moves_distribution.py
+++ src/fish_moves_distribution.py
@@ -1,6 +1,4 @@
 # Prelim function for calcualting the probability distribution for how many times fish are moved across the full batch process
-# so loop through and store the probabilities for each event
-probs = [0, 0, 0.5, 0, 0.33, 0.25, 0.2, 0.17, 0.14, 0, 0.125, 0.11, 0.1, 0.091, 0.083, 0.077]
 
 def convolve_binomial(probabilities):
     # p is a list of probabilities of Bernoulli distributions.
@@ -11,5 +9,3 @@ def convolve_binomial(probabilities):
     for p in probabilities:
         z = [(1 - p) * z[i] + p * z[i - 1] for i in range(n)]
     return z
-
-z = convolve_binomial(probs)
