a9285d9e5669ee0cfd181b57fd1d547062d27b7b
seenstevo <seenstevo@protonmail.com>
2023-05-10 22:39:02 +0200

    fishfarm class refactored

--- src/fishfarm.py
+++ src/fishfarm.py
@@ -12,6 +12,7 @@ class BaseBatch():
         self.max_stock_den = max_stock_den
         self.maxmin_stock_den = maxmin_stock_den
         self.tank_vol = tank_vol
+        self.weeks = 0
         self.stocking_den = None
         self.move = True
         self.n_tanks = self.initial_n_tanks()
@@ -46,76 +47,94 @@ class BaseBatch():
         n_fish_remove_tank = prev_step_n_fish_tank - self.n_fish_tank
         return n_fish_remove_tank
     
+    def get_starting_density(self):
+        '''
+        
+        '''
+        # save the previous weight
+        previous_weight = self.weight
+        # first find the t value based on weight before time step
+        t = time_from_weight(self.weight)
+        # update the weight after 1 week growth
+        self.weight = round(weight_from_time(t + 1), 2)
+        # max fish are needed per tank
+        max_n_fish_tank = int(n_for_max_density(self.max_stock_den, self.weight, self.tank_vol))
+        # update the number of tanks needed
+        self.n_tanks = ceil(BaseBatch.BATCH_SIZE / max_n_fish_tank)
+        # and divide the fish accordingly
+        self.n_fish_tank = int(BaseBatch.BATCH_SIZE / self.n_tanks)
+        # get the actual stocking density at start and end of time step
+        start_den = round(((self.n_fish_tank * (previous_weight / 1000)) / self.tank_vol), 2)
+        
+        return start_den, t, previous_weight
+    
+    def two_week_setup(self, t, previous_weight):
+        # update the weight after 2 weeks growth
+        two_week_weight = round(weight_from_time(t + 2), 2)
+        # max fish are needed per tank
+        max_n_fish_tank = int(n_for_max_density(self.max_stock_den, two_week_weight, self.tank_vol))
+        # update the number of tanks needed
+        self.n_tanks = ceil(BaseBatch.BATCH_SIZE / max_n_fish_tank)
+        # and divide the fish accordingly
+        self.n_fish_tank = int(BaseBatch.BATCH_SIZE / self.n_tanks)
+        # get the actual stocking density at start and end of time step
+        start_den = round(((self.n_fish_tank * (previous_weight / 1000)) / self.tank_vol), 2)
+        # now set move to False so we simply get densities for next time step
+        self.move = False
+        
+        return start_den
+    
+    
+    def no_move(self):
+        '''
+        When we are not moving the batch, we simply calculate the new weight at week end, and the new densities
+        We reset back self.move to True to carry out the move the following week
+        '''
+        # save the previous weight
+        previous_weight = self.weight
+        # first find the t value based on weight before time step
+        t = time_from_weight(self.weight)
+        # update the weight after 1 week growth
+        self.weight = round(weight_from_time(t + 1), 2)
+        # get the actual stocking density at start and end of time step
+        start_den = round(((self.n_fish_tank * (previous_weight / 1000)) / self.tank_vol), 2)
+        end_den = round(((self.n_fish_tank * (self.weight / 1000)) / self.tank_vol), 2)
+        self.stocking_den = (start_den, end_den)
+        # now set move back to True so we re-calculate the move
+        self.move = True
+
+        return self.n_fish_tank, self.weight, self.n_tanks, self.stocking_den
+    
     
     
 class BatchHotHouse(BaseBatch):
     
     MAX_WEEKS = variables.hothhouse_weeks
     
-    def __init__(self, weight, max_stock_den, maxmin_stock_den, tank_vol):
-        super().__init__(weight, max_stock_den, maxmin_stock_den, tank_vol)
-        self.weeks = 0  # initialise weeks to 0
-        
-    
     def week_step_updates(self):
         '''
         Carry out time step of one week and update values for self.weight, self.n_fish_tank and self.n_tanks
         '''
+        self.weeks += 1
+        
         if self.move:
-            # save the previous weight
-            previous_weight = self.weight
-            # first find the t value based on weight before time step
-            t = time_from_weight(self.weight)
-            # update the weight after 1 week growth
-            self.weight = round(weight_from_time(t + 1), 2)
-            # max fish are needed per tank
-            max_n_fish_tank = int(n_for_max_density(self.max_stock_den, self.weight, self.tank_vol))
-            # update the number of tanks needed
-            self.n_tanks = ceil(BaseBatch.BATCH_SIZE / max_n_fish_tank)
-            # and divide the fish accordingly
-            self.n_fish_tank = int(BaseBatch.BATCH_SIZE / self.n_tanks)
-            # get the actual stocking density at start and end of time step
-            start_den = round(((self.n_fish_tank * (previous_weight / 1000)) / self.tank_vol), 2)
+            
+            start_den, t, previous_weight = self.get_starting_density()
+            
             # if starting density is too high (using a max min) we will thin fish for 2 weeks growth
             # also checking if we have two weeks left in the hot house
-            if (start_den > self.maxmin_stock_den) and (self.weeks < (BatchHotHouse.MAX_WEEKS - 1)):
-                # update the weight after 2 weeks growth
-                tw_weight = round(weight_from_time(t + 2), 2)
-                # max fish are needed per tank
-                max_n_fish_tank = int(n_for_max_density(self.max_stock_den, tw_weight, self.tank_vol))
-                # update the number of tanks needed
-                self.n_tanks = ceil(BaseBatch.BATCH_SIZE / max_n_fish_tank)
-                # and divide the fish accordingly
-                self.n_fish_tank = int(BaseBatch.BATCH_SIZE / self.n_tanks)
-                # get the actual stocking density at start and end of time step
-                start_den = round(((self.n_fish_tank * (previous_weight / 1000)) / self.tank_vol), 2)
-                # now set move to False so we simply get densities for next time step
-                self.move = False
+            if (start_den > self.maxmin_stock_den) and (self.weeks < (BatchHotHouse.MAX_WEEKS)):
+                
+                start_den = self.two_week_setup(self, t, previous_weight)
 
-            # update the weeks by 1
-            self.weeks = self.weeks + 1
             end_den = round(((self.n_fish_tank * (self.weight / 1000)) / self.tank_vol), 2)
             self.stocking_den = (start_den, end_den)
 
             return self.n_fish_tank, self.weight, self.n_tanks, self.stocking_den
         
         else:
-            # save the previous weight
-            previous_weight = self.weight
-            # first find the t value based on weight before time step
-            t = time_from_weight(self.weight)
-            # update the weight after 1 week growth
-            self.weight = round(weight_from_time(t + 1), 2)
-            # get the actual stocking density at start and end of time step
-            start_den = round(((self.n_fish_tank * (previous_weight / 1000)) / self.tank_vol), 2)
-            end_den = round(((self.n_fish_tank * (self.weight / 1000)) / self.tank_vol), 2)
-            self.stocking_den = (start_den, end_den)
-            # update the weeks by 1
-            self.weeks = self.weeks + 1
-            # now set move back to True so we re-calculate the move
-            self.move = True
-
-            return self.n_fish_tank, self.weight, self.n_tanks, self.stocking_den
+            self.no_move()
+          
           
     
 class BatchJacks(BaseBatch):
@@ -125,34 +144,13 @@ class BatchJacks(BaseBatch):
         Carry out time step of one week and update values for self.weight, self.n_fish_tank and self.n_tanks
         '''
         if self.move:
-            # save the previous weight
-            previous_weight = self.weight
-            # first find the t value based on weight before time step
-            t = time_from_weight(self.weight)
-            # update the weight after 1 week growth
-            self.weight = round(weight_from_time(t + 1), 2)
-            # max fish are needed per tank
-            max_n_fish_tank = int(n_for_max_density(self.max_stock_den, self.weight, self.tank_vol))
-            # update the number of tanks needed
-            self.n_tanks = ceil(BaseBatch.BATCH_SIZE / max_n_fish_tank)
-            # and divide the fish accordingly
-            self.n_fish_tank = int(BaseBatch.BATCH_SIZE / self.n_tanks)
-            # get the actual stocking density at start and end of time step
-            start_den = round(((self.n_fish_tank * (previous_weight / 1000)) / self.tank_vol), 2)
+            
+            start_den, t, previous_weight = self.get_starting_density()
+            
             # if starting density is too high (using a max min) we will thin fish for 2 weeks growth
             if start_den > self.maxmin_stock_den:
-                # update the weight after 2 weeks growth
-                tw_weight = round(weight_from_time(t + 2), 2)
-                # max fish are needed per tank
-                max_n_fish_tank = int(n_for_max_density(self.max_stock_den, tw_weight, self.tank_vol))
-                # update the number of tanks needed
-                self.n_tanks = ceil(BaseBatch.BATCH_SIZE / max_n_fish_tank)
-                # and divide the fish accordingly
-                self.n_fish_tank = int(BaseBatch.BATCH_SIZE / self.n_tanks)
-                # get the actual stocking density at start and end of time step
-                start_den = round(((self.n_fish_tank * (previous_weight / 1000)) / self.tank_vol), 2)
-                # now set move to False so we simply get densities for next time step
-                self.move = False
+                
+                start_den = self.two_week_setup(self, t, previous_weight)
                 
             end_den = round(((self.n_fish_tank * (self.weight / 1000)) / self.tank_vol), 2)
             self.stocking_den = (start_den, end_den)
@@ -160,19 +158,6 @@ class BatchJacks(BaseBatch):
             return self.n_fish_tank, self.weight, self.n_tanks, self.stocking_den
         
         else:
-            # save the previous weight
-            previous_weight = self.weight
-            # first find the t value based on weight before time step
-            t = time_from_weight(self.weight)
-            # update the weight after 1 week growth
-            self.weight = round(weight_from_time(t + 1), 2)
-            # get the actual stocking density at start and end of time step
-            start_den = round(((self.n_fish_tank * (previous_weight / 1000)) / self.tank_vol), 2)
-            end_den = round(((self.n_fish_tank * (self.weight / 1000)) / self.tank_vol), 2)
-            self.stocking_den = (start_den, end_den)
-            # now set move back to True so we re-calculate the move
-            self.move = True
-
-            return self.n_fish_tank, self.weight, self.n_tanks, self.stocking_den
+            self.no_move()
     
 
